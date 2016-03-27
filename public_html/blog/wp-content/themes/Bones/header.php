c793248eaf733e68083bee492214ab5aee9b4ebb
mitu <mitu2687sp@gmail.com>
2016-03-27 23:37:12 +0900

    successtテンプレート作成

---
+++ public_html/blog/wp-content/themes/Bones/header.php
@@ -0,0 +1,57 @@
+<!doctype html>
+
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
+	<head>
+		<meta charset="utf-8">
+
+		<?php // force Internet Explorer to use the latest rendering engine available ?>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<title><?php wp_title(''); ?></title>
+
+		<?php // mobile meta (hooray!) ?>
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
+		<!--[if IE]>
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<![endif]-->
+		<?php // or, set /favicon.ico for IE10 win ?>
+		<meta name="msapplication-TileColor" content="#f01d4f">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+            <meta name="theme-color" content="#121212">
+
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<script>(function(){var obj_js=document.createElement("script");var obj_tag=document.getElementsByTagName("script")[0];obj_js.src="//www.call-ma.com/gearjs/gear.js?CD=e72cee89e7aa33362c1103c622392e43-120-1225824106&RE="+encodeURIComponent(location.search.substring(1));obj_tag.parentNode.insertBefore(obj_js,obj_tag)}());window.onload=function(){if(typeof SGEAR==="undefined"||SGEAR.getCallTel()==""){SGEAR={getCallTel:function(){return"0120165019"},getDispTel:function(){return"0120-165-019"}};var replaceTags=document.getElementsByClassName("callma_tel");for(i=0;i<replaceTags.length;i++){replaceTags[i].innerHTML=SGEAR.getDispTel()}if(typeof Ext_Callma_Init !== "undefined"){Ext_Callma_Init();}}};</script>
+
+		<?php // wordpress head functions ?>
+		<?php // end of wordpress head ?>
+
+		<?php // drop Google Analytics Here ?>
+		<?php // end analytics ?>
+
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	<meta name="description" content="住宅ローンを払えなくなった方を対象に、専門的な法律相談や解決までのサポートを行う弁護士法人です。転職・休職、離婚、事業の失敗などにより滞納・延滞が発生しお悩みを抱えている方はぜひご無料相談にお申し込みください。競売・任意売却のご相談も承ります。">
+<title><?php wp_title(' | ', true, 'right'); ?><?php bloginfo('name'); ?></title>
+		<link rel="stylesheet" type="text/css" href="/css/bootstrap-datepicker.min.css"/>        <link rel="stylesheet" type="text/css" href="/css/bootstrap-datetimepicker.min.css"/>
+	<script type="text/javascript" src="/js/bootstrap.min.js"></script> 	
+
+
+
+<style type="text/css">
+
+</style>
+
+		<?php wp_head(); ?>
+
+
+
