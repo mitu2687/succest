4b0b88422c9b231e4b8781d6025c4c641db66299
mitu <mitu2687sp@gmail.com>
2016-03-29 05:42:08 +0900

    bones function 修正

--- public_html/blog/wp-content/themes/Bones/searchform.php
+++ public_html/blog/wp-content/themes/Bones/searchform.php
@@ -1,9 +1,9 @@
 <form role="search" method="get" id="searchform" class="searchform  form-inline" action="<?php echo home_url( '/' ); ?>">
-    <div class="form-group col-md-8" style="margin-bottom: 15px;">
+    <div class="form-group " style="margin-bottom: 15px;">
         <label for="s" class="screen-reader-text"><?php _e('Search for:','bonestheme'); ?></label>
         <input class="form-control" type="text" id="s" name="s" value="" />
 
         <?php $b_block = is_mobile()? "btn-block sp-top" : "" ?>
-        <button class="btn btn-primary <?php print $b_block;?>" type="submit" id="searchsubmit" ><?php _e('Search','bonestheme'); ?></button>
+        <button class="btn btn-primary btn-sm <?php print $b_block;?>" type="submit" id="searchsubmit" ><?php _e('Search','bonestheme'); ?></button>
     </div>
 </form>
